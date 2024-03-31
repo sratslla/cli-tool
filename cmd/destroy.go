429de1a393fc0d2412ab4375bb271be1f395c18d
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-03-31 21:55:29 +0530

    manifest updated
    Signed-off-by: sratslla <shubhgaur.cse24@jecrc.ac.in>

---
+++ cmd/destroy.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+// destroyCmd represents the destroy command
+var destroyCmd = &cobra.Command{
+	Use:   "destroy",
+	Short: "Delete the Kubernetes Resources",
+	Long:  "Delete the Kubernetes Resources",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("destroy called")
+		REPO_URL := "https://raw.githubusercontent.com/sratslla/cli-tool/main"
+		YAML_FILE_PATH := "manifests/kubernetes-manifests.yaml"
+		YAML_FILE_PATH2 := "manifests/loadgenerator_ui.yaml"
+		err := destroyManifestFromGitHub(REPO_URL, YAML_FILE_PATH)
+		if err != nil {
+			fmt.Println("Error deleting manifest:", err)
+			os.Exit(1)
+		}
+		err2 := destroyManifestFromGitHub(REPO_URL, YAML_FILE_PATH2)
+		if err2 != nil {
+			fmt.Println("Error deleting manifest:", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(destroyCmd)
+}
+func destroyManifestFromGitHub(repoURL, yamlFilePath string) error {
+	cmd := exec.Command("kubectl", "delete", "-f", fmt.Sprintf("%s/%s", repoURL, yamlFilePath))
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error applying manifest: %v\n%s", err, output)
+	}
+	fmt.Println("Resources Deleted Successfully.")
+	return nil
+}
--- cmd/start.go
+++ cmd/start.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/spf13/cobra"
@@ -24,6 +25,19 @@ var startCmd = &cobra.Command{
 		} else {
 			fmt.Println("Kubernetes cluster is not running or accessible")
 			// Handle the case where Kubernetes cluster is not available
+		}
+		REPO_URL := "https://raw.githubusercontent.com/sratslla/cli-tool/main"
+		YAML_FILE_PATH := "manifests/kubernetes-manifests.yaml"
+		YAML_FILE_PATH2 := "manifests/loadgenerator_ui.yaml"
+		err := applyManifestFromGitHub(REPO_URL, YAML_FILE_PATH)
+		if err != nil {
+			fmt.Println("Error applying manifest:", err)
+			os.Exit(1)
+		}
+		err2 := applyManifestFromGitHub(REPO_URL, YAML_FILE_PATH2)
+		if err2 != nil {
+			fmt.Println("Error applying manifest:", err)
+			os.Exit(1)
 		}
 	},
 }
@@ -45,4 +59,16 @@ func isKubernetesClusterRunning() bool {
 	// If the command executed successfully, assume Kubernetes cluster is running
 	fmt.Println(cmd, output.String())
 	return true
+}
+
+func applyManifestFromGitHub(repoURL, yamlFilePath string) error {
+	cmd := exec.Command("kubectl", "apply", "-f", fmt.Sprintf("%s/%s", repoURL, yamlFilePath))
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error applying manifest: %v\n%s", err, output)
+	}
+	fmt.Println("Manifest applied successfully.", output.String())
+	return nil
 }
